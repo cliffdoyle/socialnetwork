d4b291608b555fedc035f911a01fb61f2afc6b0e
clomollo <oyoocliff471@gmail.com>
2024-09-15 14:19:18 +0300

    initial commit

---
+++ route_main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(w, r)
+
+		if err != nil {
+			generateHTML(w, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(w, threads, "layout", "public.navbar", "index")
+		}
+	}
+
+}
